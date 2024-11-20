8c825ee00e2703ede8e9a5a8326b6cb9b3b38605
zhaoccv <76190365+ZHAOCHENGV@users.noreply.github.com>
2024-11-20 22:44:58 +0800

    新增基础角色

---
+++ Source/Aura/Private/AuraCharacterBase.cpp
@@ -0,0 +1,20 @@
+
+
+
+#include "AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
