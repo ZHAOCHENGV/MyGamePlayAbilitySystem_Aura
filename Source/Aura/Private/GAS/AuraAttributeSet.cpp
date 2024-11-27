1a7b29a0c4323c64b126b502a5fd0611e5392692
zhaoccv <76190365+ZHAOCHENGV@users.noreply.github.com>
2024-11-27 22:59:13 +0800

    使用属性访问器，初始化变量

--- Source/Aura/Private/GAS/AuraAttributeSet.cpp
+++ Source/Aura/Private/GAS/AuraAttributeSet.cpp
@@ -3,3 +3,58 @@
 
 #include "GAS/AuraAttributeSet.h"
 
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UAuraAttributeSet::UAuraAttributeSet()
+{
+	//ATTRIBUTE_ACCESSORS宏自动创建的初始化函数
+	InitHealth(100.f);
+	InitMaxHealth(GetHealth());
+	InitMana(100.f);
+	InitMaxMana(GetMana());
+}
+
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	//DOREPLIFETIME_CONDITION_NOTIFY 是一个宏，用于添加对指定属性的复制行为。
+	//UAuraAttributeSet 是类名。
+	//Health 是要复制的属性。
+	//COND_None 表示没有特殊的复制条件。
+	//REPNOTIFY_Always 表示在属性变化时总是会通知。
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Health,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Mana,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxMana,COND_None,REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	//处理属性在网络上变化后的通知逻辑。
+	//GAMEPLAYATTRIBUTE_REPNOTIFY 宏用于处理属性更新，确保任何需要更新的引用（如UI）能正确响应属性的变化。
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Health,OldHealth);
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	//处理属性在网络上变化后的通知逻辑。
+	//GAMEPLAYATTRIBUTE_REPNOTIFY 宏用于处理属性更新，确保任何需要更新的引用（如UI）能正确响应属性的变化。
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,MaxHealth,OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	//处理属性在网络上变化后的通知逻辑。
+	//GAMEPLAYATTRIBUTE_REPNOTIFY 宏用于处理属性更新，确保任何需要更新的引用（如UI）能正确响应属性的变化。
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Mana,OldMana);
+}
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	//处理属性在网络上变化后的通知逻辑。
+    //GAMEPLAYATTRIBUTE_REPNOTIFY 宏用于处理属性更新，确保任何需要更新的引用（如UI）能正确响应属性的变化。
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,MaxMana,OldMaxMana);
+}
+
+
--- Source/Aura/Public/GAS/AuraAttributeSet.h
+++ Source/Aura/Public/GAS/AuraAttributeSet.h
@@ -3,8 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "AuraAttributeSet.generated.h"
+
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +21,56 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
+	//构造函数
+	UAuraAttributeSet();
+
+	//这是一个虚拟函数，用于定义哪些属性需要在网络中进行复制（replication）。
+	//通过重写这个函数，开发者可以指定哪些属性在网络同步时需要被管理。
+	// 是 Unreal Engine 用于存储和管理需要复制的属性的数组。
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	//创建健康值 ,ReplicatedUsing = OnRep_Health 指定当该属性在网络上被复制时，它会调用 OnRep_Health 方法来通知任何相关的对象或系统。
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing= OnRep_Health,Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	//属性器访问。
+	//这个宏会自动创建初始化，get,set，等方法
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,Health);
+	
+	//创建最大健康值 ,ReplicatedUsing = OnRep_MaxHealth,指定当该属性在网络上被复制时，它会调用 OnRep_Health 方法来通知任何相关的对象或系统。
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing= OnRep_MaxHealth,Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	//这个宏会自动创建初始化，get,set，等方法
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,MaxHealth);
+
+	//Rep_Health 方法会在 Health 属性在网络上复制并被修改时调用。
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	
+	//Rep_Health 方法会在 MaxHealth 属性在网络上复制并被修改时调用。
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+
+	//创建魔法值 ,ReplicatedUsing = OnRep_Health 指定当该属性在网络上被复制时，它会调用 OnRep_Health 方法来通知任何相关的对象或系统。
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing= OnRep_Mana,Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+	//这个宏会自动创建初始化，get,set，等方法
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,Mana);
+	//创建最大魔法值 ,ReplicatedUsing = OnRep_MaxHealth,指定当该属性在网络上被复制时，它会调用 OnRep_Health 方法来通知任何相关的对象或系统。
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing= OnRep_MaxMana,Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+	//这个宏会自动创建初始化，get,set，等方法
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,MaxMana);
+
+
+	
+	//Rep_Health 方法会在 Mana 属性在网络上复制并被修改时调用。
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	
+	//Rep_Health 方法会在 MaxMana 属性在网络上复制并被修改时调用。
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 	
 };
