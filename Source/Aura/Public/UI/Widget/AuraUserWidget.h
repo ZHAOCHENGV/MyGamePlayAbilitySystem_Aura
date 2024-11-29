d45219fb5459f325d4db855499b35a81681a1794
zhaoccv <76190365+ZHAOCHENGV@users.noreply.github.com>
2024-11-29 23:01:55 +0800

    添加UI基础进度条

---
+++ Source/Aura/Public/UI/Widget/AuraUserWidget.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	//BlueprintCallable  蓝图可调用函数
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetController(UObject* InWidgetController);
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UObject> WidgetController;
+
+protected:
+	//BlueprintImplementableEvent  蓝图可实现的事件
+	UFUNCTION(BlueprintImplementableEvent)
+	void WidgetControllerSet();
+	
+};
