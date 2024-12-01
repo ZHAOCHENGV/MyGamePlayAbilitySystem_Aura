4f3c6d69a7a7a12bfbb55554bf81d055dd9f99b9
zhaoccv <76190365+ZHAOCHENGV@users.noreply.github.com>
2024-12-01 15:47:58 +0800

    控件控制器构造函数

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -3,11 +3,42 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "UObject/NoExportTypes.h"
 #include "AuraWidgetController.generated.h"
 
 class UAttributeSet;
 class UAbilitySystemComponent;
+
+//BlueprintType 蓝图可使用结构体
+//定义FWidgetControllerParams类型的结构体
+USTRUCT(BlueprintType)
+struct FWidgetControllerParams
+{
+	GENERATED_BODY()
+	//默认构造函数
+	FWidgetControllerParams(){}
+	//带参数的构造函数，
+	//:PlayerController(PC), PlayerState(PS), AbilitySystemComponent(ASC), AttributeSet(AS)初始化参数
+	FWidgetControllerParams(APlayerController * PC, APlayerState * PS, UAbilitySystemComponent * ASC, UAttributeSet * AS)
+	:PlayerController(PC), PlayerState(PS), AbilitySystemComponent(ASC), AttributeSet(AS){}
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	TObjectPtr<APlayerController> PlayerController = nullptr;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	TObjectPtr<APlayerState> PlayerState = nullptr;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent = nullptr;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	TObjectPtr<UAttributeSet> AttributeSet = nullptr;
+	
+
+};
+
+
 /**
  * 
  */
@@ -15,6 +46,12 @@ UCLASS()
 class AURA_API UAuraWidgetController : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	//BlueprintCallable蓝图可调用，设置控件控制器参数
+	//设置控件控制器参数
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetControllerParams(const FWidgetControllerParams & WCParams);
 
 protected:
 	UPROPERTY(BlueprintReadOnly , Category  = "WidgetController")
