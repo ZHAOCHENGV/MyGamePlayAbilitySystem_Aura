4f2d136cbb4f4cf563bd07c9555d8ffd98726978
zhaoccv <76190365+ZHAOCHENGV@users.noreply.github.com>
2024-12-07 00:17:02 +0800

    移除游戏效果

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -40,8 +40,90 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 	// Data是一个 TSharedPtr<FGameplayEffectSpec> 类型的智能指针
 	// EffectSpecHandle.Data.Get从智能指针中提取裸指针。
 	// *EffectSpecHandle.Data.Get()解引用裸指针获取实际对象。
-	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
-	
+	//保存应用效果到一个变量中
+	FActiveGameplayEffectHandle ActiveEffectHandle = TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+
+	//获取效果的持续时间类型是否为永久,如果等于永久返回True
+	//EffectSpecHandle.Data.Get() 从句柄中获取 FGameplayEffectSpec 的指针。
+	//->Def.Get()通过 FGameplayEffectSpec 获取定义信息 UGameplayEffect。
+	//->DurationPolicy 从特效定义中访问其持续时间策略。
+	const bool bIsInfinite	= EffectSpecHandle.Data.Get()->Def.Get()->DurationPolicy == EGameplayEffectDurationType::Infinite;
+	//如果持续效果为永久和无限持续时间的游戏效果的移除政策为 重叠结束时移除
+	if (bIsInfinite && InfiniteEEffectRemovalPolicy==EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		//把效果和ASC组件，存储到映射中
+		ActiveEffectHandles.Add(ActiveEffectHandle,TargetASC);
+	}
+
+
+}
+
+void AAuraEffectActor::OnOverlap(AActor* TargetActor)
+{
+	//如果即时效果应用程序策略 等于 重叠，则应用游戏效果
+	if (InstantEffectApplicationPolicy==EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor,InstantGameplayEffectClass);
+	}
+	//如果持续效果应用程序策略 等于 重叠，则应用游戏效果
+	if (DurationEffectApplicationPolicy ==EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor,DurationGameplayEffectClass);
+	}
+	//如果无限效果应用程序策略 等于 重叠，则应用游戏效果
+	if (InfiniteEffectApplicationPolicy ==EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor,InfiniteGameplayEffectClass);
+	}
+}
+
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	//如果即时效果应用程序策略 等于 结束重叠，则应用游戏效果
+	if (InstantEffectApplicationPolicy==EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor,InstantGameplayEffectClass);
+	}
+	//如果持续效果应用程序策略 等于 结束重叠，则应用游戏效果
+	if (DurationEffectApplicationPolicy ==EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor,DurationGameplayEffectClass);
+	}
+	//如果无限效果应用程序策略 等于 重叠，则应用游戏效果
+	if (InfiniteEffectApplicationPolicy ==EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor,InfiniteGameplayEffectClass);
+	}
+	// 如果无限效果的移除策略是“重叠结束时移除”
+	if (InfiniteEEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		//获取目标Actor的能力系统组件
+		UAbilitySystemComponent * TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+		// 如果目标能力系统组件无效，则退出函数
+		if (!IsValid(TargetASC))return;
+		// 创建一个数组，用于存储需要移除的效果句柄
+		TArray<FActiveGameplayEffectHandle> HandlesToRemove;
+		// 遍历出映射中所有活动效果句柄
+		for (auto HandlePar : ActiveEffectHandles)
+		{
+			// 如果目标能力系统组件与当前句柄对应的组件相同
+			if (TargetASC == HandlePar.Value)
+			{
+				// 从目标组件中移除该效果
+				//RemoveActiveGameplayEffect 默认为 -1，会移除所有的效果，1,只会移除1个效果
+				TargetASC->RemoveActiveGameplayEffect(HandlePar.Key,1);
+				// 将已移除的效果句柄添加到待移除数组中
+				HandlesToRemove.Add(HandlePar.Key);
+			}
+		}
+		// 遍历需要移除的效果句柄
+		for (auto Handler : HandlesToRemove)
+		{
+			// 从映射中删除对应的效果句柄
+			ActiveEffectHandles.FindAndRemoveChecked(Handler);
+		}
+		
+	}
 }
 
 
