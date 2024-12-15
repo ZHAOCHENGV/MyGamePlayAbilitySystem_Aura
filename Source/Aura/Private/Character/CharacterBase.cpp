62fdaab41e9197074548a5d54b77a24cadae04c4
zhaoccv <76190365+ZHAOCHENGV@users.noreply.github.com>
2024-12-15 23:18:26 +0800

    初始化次元属性

--- Source/Aura/Private/Character/CharacterBase.cpp
+++ Source/Aura/Private/Character/CharacterBase.cpp
@@ -48,11 +48,11 @@ void ACharacterBase::ApplyEffectToSelf(TSubclassOf<UGameplayEffect> GamePlayEffe
 	// 检查 Ability System 组件是否有效
 	check(IsValid(GetAbilitySystemComponent()));
 	// 检查 GameEffectClass 是否被设置
-	check(GameEffectClass);
+	check(GamePlayEffectClass);
 	// 创建一个 Gameplay Effect 的上下文，通常用于指定应用效果的目标信息
 	const FGameplayEffectContextHandle ContextHandle = GetAbilitySystemComponent()->MakeEffectContext();
 	// 创建一个用于应用的 Gameplay Effect Spec，这里使用 GameEffectClass 作为默认效果，Level为强度，ContextHandle 为上下文
-	const FGameplayEffectSpecHandle SpecHandle = GetAbilitySystemComponent()->MakeOutgoingSpec(GameEffectClass,Level,ContextHandle);
+	const FGameplayEffectSpecHandle SpecHandle = GetAbilitySystemComponent()->MakeOutgoingSpec(GamePlayEffectClass,Level,ContextHandle);
 	// 应用这个效果到当前的 Ability System 组件（目标为当前角色）
 	GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(),GetAbilitySystemComponent());
 }
